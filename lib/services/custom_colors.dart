bb8e04ab3d3d7d651364eeb0cfd86d437095b3c6
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2022-06-19 04:02:47 +0530

    feat: add app colors

---
+++ lib/services/custom_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class CustomColors {
+  static Color appGreen = const Color(0xFF00CC8D);
+  static Color appDarkGrey = const Color(0xFF949494);
+  static Color appLightGrey = const Color(0xFFC4C4C4);
+  static Color appSecondaryGrey = const Color(0xffE5E5E5);
+}
