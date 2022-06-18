4ed4e98396131af13b86e5ae7d7413d1ca36a57b
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2022-06-19 04:58:27 +0530

    feat: set all string constants

---
+++ lib/constants/constants.dart
@@ -0,0 +1,39 @@
+// email-verification
+
+const YOUTUBE_API_KEY = 'AIzaSyDQszH2-veOqW15o7DVMZK_Mx1co4JeUWI';
+
+const nightIntroText =
+    'Try out this nightly check-in to see how you’re doing holistically. We help you keep track of healthy habits which add up overtime, to create a happier, healthier version of yourself.\n\nIt\'s beneficial to incorporate this activity into your evening routine, so that your mind and body are in sync daily.\n\nLet\'s explore how you\'re doing!';
+
+const emailSubtitle =
+    'Please check your inbox or spam\nfolder to find verification link.\n\nOnce you have verified you will\nbe allowed to continue.';
+
+// reset-password
+const resetPasswordSubtitle =
+    'Please check your inbox or spam\nfolder to find password reset link.';
+
+// forgot-password
+const forgotPasswordSubtitle =
+    'No worries! Just enter your email and \n we\'ll send you a reset password link.';
+
+const already = 'Already have an account? ';
+
+const anoy = 'I want to stay\nanonymous';
+
+const shareTitle = 'Share more about yourself';
+
+const shareSubtitle =
+    'This will help us build the best\nwellness plan for you!';
+
+final List<String> cities = [
+  'ahmedabad',
+  'bangalore',
+  'chandigarh',
+  'chennai',
+  'delhi',
+  'hyderabad',
+  'jaipur',
+  'kolkata',
+  'lucknow',
+  'mumbai'
+];
