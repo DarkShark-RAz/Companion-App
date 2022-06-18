311aed12a673e2d55a5546f2d29c8b7a3aedf750
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2022-06-19 04:07:40 +0530

    feat: make custom indicator

---
+++ lib/shared/indicator.dart
@@ -0,0 +1,19 @@
+import 'package:companion_app/services/custom_colors.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class Indicator {
+  static void showLoading() {
+    Get.dialog(
+      Center(
+        child: CircularProgressIndicator(color: CustomColors.appGreen),
+      ),
+    );
+  }
+
+  static void closeLoading() {
+    if (Get.isDialogOpen!) {
+      Get.back();
+    }
+  }
+}
