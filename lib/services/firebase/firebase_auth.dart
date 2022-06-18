44d85ce1f5ca3ab3cc7543595e31071603825720
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2022-06-19 04:49:03 +0530

    chore: firebase helper functions

---
+++ lib/services/firebase/firebase_auth.dart
@@ -0,0 +1,40 @@
+import 'package:companion_app/shared/indicator.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:get/get.dart';
+
+class FirebaseAuthentication {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<void> createAccount(String email, String password) async {
+    try {
+      await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<void> login(String email, String password) async {
+    try {
+      await _auth.signInWithEmailAndPassword(email: email, password: password);
+      Indicator.closeLoading();
+      Get.offAllNamed('/main-screen', arguments: _auth.currentUser);
+    } catch (e) {
+      print(e);
+      Indicator.closeLoading();
+      Get.snackbar("Something went wrong", e.toString());
+    }
+  }
+
+  Future<void> logOut() async {
+    try {
+      await _auth.signOut().then((value) {
+        Get.toNamed("/");
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+}
